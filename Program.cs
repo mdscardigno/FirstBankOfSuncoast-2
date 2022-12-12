3f82d860b4a16b7c5370782274ae3ae17d6b01c5
Milagro Scardigno <mdscardigno@gmail.com>
2022-12-12 12:22:36 -0500

    prompts for string and integer methods

--- Program.cs
+++ Program.cs
@@ -2,11 +2,25 @@
 
 namespace FirstBankOfSuncoast
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to First Bank Of Suncoast.");
+        }
+
+        static string PromptForString(string prompt)
+        {
+            Console.Write(prompt);
+            var userInput = Console.ReadLine();
+            return userInput;
+        }
+
+        static int PromptForInteger(string prompt)
+        {
+            var userInput = PromptForString(prompt);
+            var userInputAsInteger = int.Parse(userInput);
+            return userInputAsInteger;
+        }
     }
-  }
 }
