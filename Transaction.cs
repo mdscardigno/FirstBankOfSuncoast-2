2da907675f4e1db41686b96ff794924d7ad463c3
Milagro Scardigno <mdscardigno@gmail.com>
2022-12-12 13:19:11 -0500

    properties of class Transaction

--- Transaction.cs
+++ Transaction.cs
@@ -1,12 +1,18 @@
+using System;
+
 namespace FirstBankOfSuncoast
 {
     public class Transaction
     {
         //-Properties
         //--Amount (int) The amount of the transaction
+        public int Amount { get; set; }
         //--Type (string) The type of transaction (deposit, withdraw, transfer)
+        public string Type { get; set; }
         //--TimeStamp (DateTime) The time the transaction was created
+        public DateTime TimeStamp { get; set; }
         //--Account (string) Checking, Savings
+        public string Account { get; set; }
 
     }
 }
